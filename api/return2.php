85da7bbd937800920b0b8be0afa1edef26aa283b
PNDaniel <daniel_nunes_21@hotmail.com>
2013-11-16 23:04:00 +0000

    API v2.0

---
+++ api/return2.php
@@ -0,0 +1,174 @@
+
+<?php
+	$db = new PDO('sqlite:database\documents.db');
+
+	$InvoiceArray = array();
+
+	if(!empty($_GET['field']))
+	{				
+		$result1 = $db->query('SELECT * FROM Customer');
+		$data1 = $result1->fetchAll();
+		foreach($data1 as $row)
+		{						
+			switch ($_GET['op'])
+			{
+				case 'equal':
+				case 'contains':
+				case 'min':				
+				case 'max':
+					switch ($_GET['field'])
+					{
+						case 'CustomerID': 
+							switch ($_GET['op'])
+							{
+								case 'equal':
+									if ($row['CustomerID'] == $_GET['value1'])
+									{
+										$InvoiceArray[] = $row['CustomerID'];
+										$InvoiceArray[] = $row['CustomerTaxID'];										
+										$InvoiceArray[] = $row['CompanyName'];			
+									}
+									break;
+								case 'min':
+									if ($row['CustomerID'] >= $_GET['value1'])
+									{
+										$InvoiceArray[] = $row['CustomerID'];
+										$InvoiceArray[] = $row['CustomerTaxID'];
+										$InvoiceArray[] = $row['CompanyName'];				
+									}
+									break;
+								case 'max':
+									if ($row['CustomerID'] <= $_GET['value1'])
+									{
+										$InvoiceArray[] = $row['CustomerID'];
+										$InvoiceArray[] = $row['CustomerTaxID'];
+										$InvoiceArray[] = $row['CompanyName'];					
+									}
+									break;
+								case 'contains':
+									if(strpos($row['CustomerID'],$_GET['value1']) !== false)
+									{
+										$InvoiceArray[] = $row['CustomerID'];
+										$InvoiceArray[] = $row['CustomerTaxID'];
+										$InvoiceArray[] = $row['CompanyName'];	
+									}
+									break;
+							}
+							break;
+						case 'CustomerTaxID': 
+							switch ($_GET['op'])
+							{
+								case 'equal':
+									if ($row['CustomerTaxID'] == $_GET['value1'])
+									{
+										$InvoiceArray[] = $row['CustomerID'];
+										$InvoiceArray[] = $row['CustomerTaxID'];
+										$InvoiceArray[] = $row['CompanyName'];					
+									}
+									break;
+								case 'min':
+									if ($row['CustomerTaxID'] >= $_GET['value1'])
+									{
+										$InvoiceArray[] = $row['CustomerID'];
+										$InvoiceArray[] = $row['CustomerTaxID'];
+										$InvoiceArray[] = $row['CompanyName'];					
+									}
+									break;
+								case 'max':
+									if ($row['CustomerTaxID'] <= $_GET['value1'])
+									{
+										$InvoiceArray[] = $row['CustomerID'];
+										$InvoiceArray[] = $row['CustomerTaxID'];
+										$InvoiceArray[] = $row['CompanyName'];					
+									}
+									break;
+								case 'contains':
+									if(strpos($row['CustomerTaxID'],$_GET['value1']) !== false)
+									{
+										$InvoiceArray[] = $row['CustomerID'];
+										$InvoiceArray[] = $row['CustomerTaxID'];
+										$InvoiceArray[] = $row['CompanyName'];
+									}
+									break;
+							}
+							break;
+						case 'CompanyName': 
+							switch ($_GET['op'])
+							{
+								case 'equal':
+									if ($row['CompanyName'] == $_GET['value1'])
+									{
+										$InvoiceArray[] = $row['CustomerID'];
+										$InvoiceArray[] = $row['CustomerTaxID'];
+										$InvoiceArray[] = $row['CompanyName'];		
+									}
+									break;
+								case 'min':
+									if ($row['CompanyName'] >= $_GET['value1'])
+									{
+										$InvoiceArray[] = $row['CustomerID'];
+										$InvoiceArray[] = $row['CustomerTaxID'];
+										$InvoiceArray[] = $row['CompanyName'];					
+									}
+									break;
+								case 'max':
+									if ($row['CompanyName'] <= $_GET['value1'])
+									{
+										$InvoiceArray[] = $row['CustomerID'];
+										$InvoiceArray[] = $row['CustomerTaxID'];
+										$InvoiceArray[] = $row['CompanyName'];				
+									}
+									break;
+								case 'contains':
+									if(strpos(strtolower ($row['CompanyName']),strtolower ($_GET['value1'])) !== false)
+									{
+										$InvoiceArray[] = $row['CustomerID'];
+										$InvoiceArray[] = $row['CustomerTaxID'];
+										$InvoiceArray[] = $row['CompanyName'];
+									}
+									break;								
+							}
+							break;						
+					}
+					break;
+				case 'range':
+					switch ($_GET['field'])
+					{
+						case 'CustomerID': 
+							if ( ($row['CustomerID'] >= $_GET['value1']) && ($row['CustomerID'] <= $_GET['value2']))
+							{
+								$InvoiceArray[] = $row['CustomerID'];
+								$InvoiceArray[] = $row['CustomerTaxID'];
+								$InvoiceArray[] = $row['CompanyName'];				
+							}
+							break;
+						case 'CustomerTaxID': 
+							if ( ($row['CustomerTaxID'] >= $_GET['value1']) && ($row['CustomerTaxID'] <= $_GET['value2']))
+							{
+								$InvoiceArray[] = $row['CustomerID'];
+								$InvoiceArray[] = $row['CustomerTaxID'];
+								$InvoiceArray[] = $row['CompanyName'];				
+							}						
+							break;
+						case 'CompanyName': 
+							if ( ($row['CompanyName'] >= $_GET['value1']) && ($row['CompanyName'] <= $_GET['value2']))
+							{
+								$InvoiceArray[] = $row['CustomerID'];
+								$InvoiceArray[] = $row['CustomerTaxID'];
+								$InvoiceArray[] = $row['CompanyName'];				
+							}
+							break;
+					}
+					break;
+			}					
+		}
+	}
+		
+	if(empty($InvoiceArray)) 
+	{
+		$InvoiceArray;
+		echo json_encode($InvoiceArray);
+	}
+	else echo json_encode($InvoiceArray);		
+?>
+    
---
+++ api/searchInvoicesByField.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+
+<HTML>
+	<HEAD>	
+		<title> Sistema de faturação online </title>
+		<META http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+		<link rel="stylesheet" href="../style.css">
+		<script language="Javascript" type="text/javascript" src="http://code.jquery.com/jquery-1.9.1.js"></script>
+		<script type='text/javascript'>
+		$(document).ready(function()
+		{
+			$("input[name='op']:radio").click(function()
+			{
+				$value1 = $("input[name='value1']");
+				$var = $(this).val();
+																		
+				if ($var ==  'range')
+				{
+					if($("input[name='value2']").size() <= 0)
+					{
+						$value1.after('<input type="text" name="value2" value="">');
+						$("input[name='value2']").attr("type",$value1.attr("type"));
+					}
+				}			
+				else
+				{
+					$("input[name='value2']").remove();
+				}
+
+				if ($var == 'contains')
+				{
+					$("input[name='value1']").attr("type","text");
+				}
+			});
+			
+			$("input[name='field']:radio").click(function()
+			{
+				$value1 = $("input[name='value1']");
+				$var = $(this).val();
+																		
+				if ($var ==  'GrossTotal')
+				{
+					$("input[name='value1']").attr("type","number");
+					$("input[name='value2']").attr("type","number");
+					$("input[name='value1']").attr("step","any");
+					$("input[name='value2']").attr("step","any");
+				}			
+				else if($var ==  'InvoiceDate')
+					{
+						$("input[name='value1']").attr("type","date");
+						$("input[name='value2']").attr("type","date");
+					}
+					else
+					{
+						$("input[name='value1']").attr("type","text");
+						$("input[name='value2']").attr("type","text");
+					}
+									
+				if ($var == 'CompanyName')
+				{
+					$("input[name='op']:radio[value=min]").remove();
+					$("input[name='op']:radio[value=max]").remove();
+					$("label[for='min']").remove();
+					$("label[for='max']").remove();
+				}
+				else
+				{
+					$value1 = $("label[for='contains']");
+					
+					if ($("input[name='op']:radio[value=min]").size() <= 0 )
+					{
+						$value1.after('<input type="radio" name="op" value="max"> <label for="max">Max</label>');
+						$value1.after('<input type="radio" name="op" value="min"> <label for="min">Min</label>');																		
+					}
+					
+				}
+			});
+		});
+		</script>
+	</HEAD>
+
+	<BODY>
+	<div id="conteudo">
+		<form action="return.php" method="GET">
+			OP:
+			<input type="radio" name="op" value="range"><label for="range">Range</label>
+			<input type="radio" name="op" value="equal"><label for="equal">Equal</label>
+			<input type="radio" name="op" value="contains"><label for="contains">Contains</label>
+			<input type="radio" name="op" value="min"> <label for="min">Min</label>
+			<input type="radio" name="op" value="max"> <label for="max">Max</label>
+			<br>
+			Field: 
+			<input type="radio" name="field" value="InvoiceNo"><label for="InvoiceNo">Número da fatura</label>
+		    <input type="radio" name="field" value="InvoiceDate"><label for="InvoiceDate">Data da fatura</label>
+		    <input type="radio" name="field" value="GrossTotal"><label for="GrossTotal">Total Bruto</label>
+		    <input type="radio" name="field" value="CompanyName"><label for="CompanyName">Nome da Empresa</label>
+		    <br>
+			Value: 
+			<br>
+			<input type="text" name="value1" value="">
+			<input type="text" name="value2" value="">
+			<br>			
+			<input type="submit">
+		</form>		
+	<div>	
+	</BODY>		
+</HTML>
