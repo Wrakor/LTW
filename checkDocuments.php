ec00ebdd0a2146333d25ebecd6949ff80705ca3a
Wrakor <nunocruz59@hotmail.com>
2013-11-27 17:15:27 +0000

    faturas em modo tabela no "Consultar Documentos"

--- checkDocuments.php
+++ checkDocuments.php
@@ -55,32 +55,35 @@
   {
     $path = 'showInvoice.php?InvoiceNo=' . $row['InvoiceNo']; 
     echo '<a href=' . $path . '> ';
+
+ 
+  
   	echo '<h3 class="btab">' . '- ' . $row['InvoiceNo'] . '</h3><a>'; 
   	
-  	echo '<div class="btab">' . '<b>Data: </b>' .$row['InvoiceDate'] . '<br>';  
-  	echo '<b>ID de Cliente: </b>'  . $row['CustomerID'] . '<br></div>';  
+  	echo '<div class="btab"><table border="1"><tr> <td>' . '<b>Data: </b></td> <td>' .$row['InvoiceDate'] . '</td></tr><br>';  
+  	echo '<tr><td><b>ID de Cliente: </b></td><td>'  . $row['CustomerID'] . '</td></tr></table><br></div>';  
     echo '<div class="text">';
-    echo '<b>Linhas de Produtos:</b><br>';   
+    echo '<table border="1"> <tr> <td colspan="2"><b>Linhas de Produtos:</b><br></td></tr>';   
   
   	foreach ($data2 as $row2)
   	{
   		if ($row['id'] == $row2['idInvoice'])
   		{
-  			echo '<b class="btab">- Linha nº </b>'  . $row2['LineNumber'] . '<br>';  
-	   	 	echo '<b class="btab2">Código do Produto/Serviço:</b>'  . $row2['ProductCode'] . '<br>';  
-	    	echo '<b class="btab2">Nº de unidades vendidas:</b>'  . $row2['Quantity'] . '<br>';  
-	    	echo '<b class="btab2">Preço unitário:</b>'  . $row2['UnitPrice'] . '<br>';  
-	    	echo '<b class="btab2">Total:</b>'  . $row2['CreditAmount'] . '<br>'; 
+  			echo '<tr><td colspan="7"><b class="btab">- Linha nº </b>'  . $row2['LineNumber'] . '</td></tr>';  
+	   	 	echo '<tr><td><b class="btab2">Código do Produto/Serviço:</b></td><td>'  . $row2['ProductCode'] . '</td></tr>';  
+	    	echo '<tr><td><b class="btab2">Nº de unidades vendidas:</b></td><td>'  . $row2['Quantity'] . '</tr></td>';  
+	    	echo '<tr><td><b class="btab2">Preço unitário:</b></td><td>'  . $row2['UnitPrice'] . '</tr></td>';  
+	    	echo '<tr><td><b class="btab2">Total:</b></td><td>'  . $row2['CreditAmount'] . '</tr></td>'; 
 
-	    	echo '<b class="btab2">Taxas:</b><br>';  
-	    	echo '<b class="btab3">Tipo de Taxa:</b>'  . $row2['TaxType'] . '<br>';  
-	    	echo '<b class="btab3">Percentagem da Taxa:</b>'  . $row2['TaxPercentage'] . '%<br>';  
+	    	echo '<tr><td colspan="2"><b class="btab2">Taxas:</b></tr>';  
+	    	echo '<tr><td><b class="btab3">Tipo de Taxa:</b></td><td>'  . $row2['TaxType'] . '</tr></td>';  
+	    	echo '<tr><td><b class="btab3">Percentagem da Taxa:</b></td><td>'  . $row2['TaxPercentage'] . '%</tr></td>';  
   		}
   	}
 
-  	echo '<b>Total de Imposto: </b>'  . $row['TaxPayable'] . '<br>';  
-   	echo '<b>Total sem Imposto: </b>'  . $row['NetTotal'] . '<br>';  
-   	echo '<b>Total: </b>'  . $row['GrossTotal'] . '<br>';
+  	echo '<tr><td><b>Total de Imposto: </b></td><td>'  . $row['TaxPayable'] . '<br></tr></td>';  
+   	echo '<tr><td><b>Total sem Imposto: </b></td><td>'  . $row['NetTotal'] . '<br></tr></td>';  
+   	echo '<tr><td><b>Total: </b></td><td>'  . $row['GrossTotal'] . '<br></tr></td></table>';
     echo '</div>'; 
     echo '<h4 class="mostrar"> Ver mais </h4><br>';    
    }
