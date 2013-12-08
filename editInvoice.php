7cd744daf742f5cb2355588734a2e9c5d4284443
PNDaniel <daniel_nunes_21@hotmail.com>
2013-12-08 23:58:41 +0000

    editInvoice 2.1

--- editInvoice.php
+++ editInvoice.php
@@ -6,14 +6,27 @@
 
 	if ($_SESSION['permission'] == "reader")
 	{
-		echo ' Precisa de ter permissões de writer para editar faturas!';
+		echo 'Precisa de ter permissões de writer para editar faturas!';
 		exit();
 	}
 
 	$db = new PDO('sqlite:database/documents.db');
 
-	$Insert= $db->prepare("UPDATE Product SET ProductDescription = ?, UnitPrice = ?, UnitOfMeasure = ? WHERE ProductCode = ?");
-	$Insert->execute(array($_POST['ProductDescription'], $_POST['UnitPrice'], $_POST['UnitOfMeasure'], $_POST['ProductCode']));
+	$insert = $db->prepare("UPDATE Invoice SET InvoiceDate = ?, CustomerID = ?, TaxPayable = ?, NetTotal = ?, GrossTotal = ? WHERE InvoiceNo = ?");
+	$insert->execute(array($_POST['InvoiceDate'], $_POST['CustomerID'], $_POST['TaxPayable'], $_POST['NetTotal'], $_POST['TaxPayable'] + $_POST['NetTotal'], $_POST['InvoiceNo']));	
+	$count = count($_POST['ProductCode']);	
+	
+	for($i = 0; $i < $count ;$i++)
+	{
+		$insert = $db->prepare('UPDATE Line SET ProductCode = ?, Quantity = ?, UnitPrice = ?, CreditAmount = ?, TaxType = ?, TaxPercentage = ? WHERE LineNumber = ?');
+		$insert->execute(array($_POST['ProductCode'][$i], 
+		$_POST['Quantity'][$i],
+		$_POST['UnitPrice'][$i], 
+		$_POST['Quantity'][$i] * $_POST['UnitPrice'][$i], 
+		$_POST['TaxType'][$i], 
+		$_POST['TaxPercentage'][$i], 
+		$i+1));
+	}
 
-	echo 'Fatura editado!';
+	echo 'Fatura editada!';
 ?>
--- editInvoiceForm.php
+++ editInvoiceForm.php
@@ -22,26 +22,26 @@
 	$lines = $db->query('SELECT * FROM Line WHERE idInvoice = ?');
 	$lines->execute(array($idGet[0]));
 	$line = $lines->fetchAll();
-
+	
 	echo '<form action="editInvoice.php" method="POST">
 			<h2>Editar Fatura:</h2><br>
 			Numero de Fatura: <br><input type="text" name="InvoiceNo" value='. $invoice['InvoiceNo'] .' readonly><br><br>
-			Data da Fatura: <br><input type="date" name="InvoiceDatea" value="' . $invoice['InvoiceDate'] .'"><br><br>
+			Data da Fatura: <br><input type="date" name="InvoiceDate" value="' . $invoice['InvoiceDate'] .'"><br><br>
 			ID do Cliente: <br><input type="number" name="CustomerID" value=' . $invoice['CustomerID'] .'><br><br><br>';
-
+			$LineNumber = array();
 			foreach($line as $row)
-			{
+			{				
+				array_push($LineNumber, $row['LineNumber']);
 				echo '<br>
-					  ID do Produto: <br><input type="number" name="ProductCode" value=' . $row['ProductCode'] .'><br><br>
-					  Quantidade Vendida: <br><input type="number" name="Quantity" value=' . $row['Quantity'] .'><br><br>
-					  Pre&ccedil;o Unit&aacute;rio: <br><input type="number" name="UnitPrice" value=' . $row['UnitPrice'] .'><br><br>
-					  Total: <br><input type="number" name="CreditAmount" value=' . $row['CreditAmount'] .'><br><br>
-					  Tipo de Taxa: <br><input type="text" name="TaxType" value=' . $row['TaxType'] .'><br><br>
-					  Percentagem da Taxa: <br><input type="text" name="TaxPercentage" value=' . $row['TaxPercentage'] .'>%<br><br><br>';
+					  Numero da Linha: <br><input type="text" name="LineNumber" value='. $row['LineNumber'] .' readonly><br><br>
+					  ID do Produto: <br><input type="number" name="ProductCode[]" value=' . $row['ProductCode'] .'><br><br>
+					  Quantidade Vendida: <br><input type="number" name="Quantity[]" value=' . $row['Quantity'] .'><br><br>
+					  Pre&ccedil;o Unit&aacute;rio: <br><input type="number" name="UnitPrice[]" value=' . $row['UnitPrice'] .'><br><br>					  
+					  Tipo de Taxa: <br><input type="text" name="TaxType[]" value=' . $row['TaxType'] .'><br><br>
+					  Percentagem da Taxa: <br><input type="text" name="TaxPercentage[]" value=' . $row['TaxPercentage'] .'>%<br><br><br>';
 			}						
 	echo '<br>Total de Imposto: <br><input type="number" name="TaxPayable" value=' . $invoice['TaxPayable'] .'><br><br>
 			Total sem Imposto: <br><input type="number" name="NetTotal" value=' . $invoice['NetTotal'] .'><br><br>
-			Total : <br><input type="number" name="GrossTotal" value=' . $invoice['GrossTotal'] .'><br><br>
 			<input type="submit" value="Editar Fatura">
 		</form>';
 
